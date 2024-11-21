7cac70fa6cda16da908cbc5918d42d4a17acd7e4
user <terrydjony@gmail.com>
2024-11-21 23:20:25 +0700

    finish one article

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/dton/create-ton-telegram-bot-with-dton-webhook.md">
+            to="/docs/dton/create-ton-telegram-bot-with-dton-webhook">
             Create TON Telegram Bot using DTon
           </Link>
         </div>
